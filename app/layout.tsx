ce6d8bf612e959c0ebfa5b6d021bdd7c0dd37182
kaushik-codal <ksathvara@codal.com>
2024-05-13 10:12:26 +0530

    added script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Flex } from "antd";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="clarity">{`
+      <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "maxin74jir");
+      </script>
+      `}</Script>
       <body>
         <Flex gap={8} vertical className="p-6">
           {children}

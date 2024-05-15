35533def325559a7f38b884f82ed4cc23399e856
kaushiksathvara <ksathvara@codal.com>
2024-05-15 10:09:16 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,18 +19,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script id="clarity">{`
-      <script type="text/javascript">
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "maxin74jir");
-      </script>
-      `}</Script>
       <body>
         <Flex gap={8} vertical className="p-6">
           {children}
+          <Script
+            id="microsoft-clarity-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "maxin74jir");
+              `,
+            }}
+          />
         </Flex>
       </body>
     </html>
